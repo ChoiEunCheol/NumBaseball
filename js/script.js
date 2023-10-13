049b135a693639765a8a91834e501a46b89369e0
최은철 <wndqhrgkdlel@gmail.com>
2023-10-13 11:42:42 +0900

    input에 id로 접근

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 const left = document.getElementById('left');
 const best = document.getElementById('best');
 const count = document.getElementById('count');
+const input = document.getElementById('input');
