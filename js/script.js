3622995c35b78b28d994e0fbb9668701d5bb0f9d
최은철 <wndqhrgkdlel@gmail.com>
2023-10-13 11:52:25 +0900

    같은 방법으로 b와 c생성 후 출력 확인

--- js/script.js
+++ js/script.js
@@ -4,11 +4,15 @@ const count = document.getElementById('count');
 const input = document.getElementById('input');
 
 //랜덤한 3자리 숫자 생성 , 3자리 숫자는 100*a + 10*b + c 의 형태로 구성
+let a = Math.floor(Math.random()*10);
+let b = Math.floor(Math.random()*10);
+let c = Math.floor(Math.random()*10);
 
 // 엔터키 입력 시 이벤트 설정
 window.addEventListener('keydown',e => {
     if (e.key === "Enter"){
-        let a = Math.floor(Math.random()*10)
         console.log(a);
+        console.log(b);
+        console.log(c);
     }
-})
+});
