9f0d155bc2b55f18dabfafa1f0177c9b3e022e5a
최은철 <wndqhrgkdlel@gmail.com>
2023-10-13 11:56:24 +0900

    a는 0이 될 수 없기 때문에 범위 재지정 및 출력 확인

--- js/script.js
+++ js/script.js
@@ -4,19 +4,15 @@ const count = document.getElementById('count');
 const input = document.getElementById('input');
 
 //랜덤한 3자리 숫자 생성 , 3자리 숫자는 100*a + 10*b + c 의 형태로 구성
-function createRandomNumber(){
-    let a = Math.floor(Math.random()*10);
+    let a = Math.floor(Math.random()*9+1);
     let b = Math.floor(Math.random()*10);
     let c = Math.floor(Math.random()*10);
-};
-
-createRandomNumber();
 
 // 엔터키 입력 시 이벤트 설정
 window.addEventListener('keydown',e => {
     if (e.key === "Enter"){
+        let a = Math.floor(Math.random()*9+1);
+
         console.log(a);
-        console.log(b);
-        console.log(c);
     }
 });
