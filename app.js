cd8fd9657e40bfc365b7bb0978435a119aeb7870
최은철 <wndqhrgkdlel@gmail.com>
2023-10-13 12:16:01 +0900

    실험용 단순한 서버 생성

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ const contextType = {
     'Context-Type' : 'text/html',
     'charset' : 'utf-8'
 };
+
+// 서버 생성
+const server = http.createServer((request,response)=>{
+    response.writeHead(200, contextType);
+    response.end("ㅇㅇ");
+})
