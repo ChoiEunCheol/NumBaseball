dbc7a515be262f9d57668f468af66fd5b2479748
최은철 <wndqhrgkdlel@gmail.com>
2023-10-13 11:44:49 +0900

    키 입력 이벤트 테스트

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ const left = document.getElementById('left');
 const best = document.getElementById('best');
 const count = document.getElementById('count');
 const input = document.getElementById('input');
+
+window.addEventListener('keydown',e => {
+    console.log(e);
+})
