e97923ace1e3d2860cab63abcf72c3a77b957596
최은철 <wndqhrgkdlel@gmail.com>
2023-10-13 16:34:09 +0900

    server가 js파일을 못 읽어 branch를 만들고 express를 실험할 예정

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 // 내장모듈
 const http = require('http');
+const fs = require('fs');
 
 // 타입 설정
 const contextType = {
     'Content-Type': 'text/html; charset=utf-8'
 };
 
+let data = fs.readFileSync('./index.html',contextType);
+
 // 서버 생성
 const server = http.createServer((request,response)=>{
     if(request.method === 'GET' && request.url === '/'){
         response.writeHead(200, contextType);
-        response.end("ㅇㅇ");
+        response.end(data);
+    } else{
+        response.writeHead(404, contextType);
+        response.end("주소 확인!");
     }
-    if(request.method === 'GET' && request.url === '/1'){
-        response.writeHead(200, contextType);
-        response.end("ㄴㄴ");}
 })
 
 server.listen(8080);
