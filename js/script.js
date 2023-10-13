d13d364e48a8de2b1e4f8336802b8bef1fdc2869
최은철 <wndqhrgkdlel@gmail.com>
2023-10-13 11:50:20 +0900

    a라는 변수 생성 후 랜덤으로 출력되는지 확인

--- js/script.js
+++ js/script.js
@@ -3,8 +3,12 @@ const best = document.getElementById('best');
 const count = document.getElementById('count');
 const input = document.getElementById('input');
 
+//랜덤한 3자리 숫자 생성 , 3자리 숫자는 100*a + 10*b + c 의 형태로 구성
+
+// 엔터키 입력 시 이벤트 설정
 window.addEventListener('keydown',e => {
     if (e.key === "Enter"){
-        alert(input.value);
+        let a = Math.random()
+        console.log(a);
     }
 })
