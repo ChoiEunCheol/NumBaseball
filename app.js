118a0a32ec38d92379534cbd6d640018718214a7
ChoiEunCheol <wndqhrgkdlel@gmail.com>
2023-10-14 22:52:11 +0900

    폴더에 있는 js 파일도 전송됨을 확인

--- app.js
+++ app.js
@@ -7,18 +7,17 @@ const contextType = {
     'Content-Type': 'text/html; charset=utf-8'
 };
 
-let data = fs.readFileSync('./index.html',contextType);
+// let data = fs.readFileSync('./index.html',contextType);
 
 // 서버 생성
 const server = http.createServer((request,response)=>{
     if(request.method === 'GET' && request.url === '/'){
         response.writeHead(200, contextType);
-        response.end(data);
+        response.end(fs.readFileSync('./js/script.js',contextType));
     } else{
         response.writeHead(404, contextType);
         response.end("주소 확인!");
     }
 })
-
 
 server.listen(8080);
