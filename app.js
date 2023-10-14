9a3fd39619fd7060537a47a1a93e329915bd8d10
ChoiEunCheol <wndqhrgkdlel@gmail.com>
2023-10-14 22:54:00 +0900

    branch 만들기 전 세팅

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const contextType = {
     'Content-Type': 'text/html; charset=utf-8'
 };
 
-// let data = fs.readFileSync('./index.html',contextType);
+let data = fs.readFileSync('./index.html',contextType);
 
 // 서버 생성
 const server = http.createServer((request,response)=>{
     if(request.method === 'GET' && request.url === '/'){
         response.writeHead(200, contextType);
-        response.end(fs.readFileSync('./style/style.css',contextType));
+        response.end(data);
     } else{
         response.writeHead(404, contextType);
         response.end("주소 확인!");
